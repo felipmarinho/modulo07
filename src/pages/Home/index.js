92e64e7465ce9f5543f982480c27178abd47c9bb
Felipe Marinho <felipmarinho@gmail.com>
2019-12-09 04:51:31 -0300

    Ajustando o estilo da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,107 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-	return <h1>Home</h1>;
+	return (
+		<ProductList>
+			<li>
+				<img
+					src="https://http2.mlstatic.com/tnis-nike-free-rn-flyknit-2018-original-D_NQ_NP_609798-MLB30088101460_042019-F.jpg"
+					alt="tenis"
+				/>
+				<strong>Tênis nike free rn flyknit 2018</strong>
+				<span>R$179,00</span>
+
+				<button type="button">
+					<div>
+						<MdAddShoppingCart size={16} color="#FFF" /> 3
+					</div>
+
+					<span>ADICIONAR AO CARRINHO</span>
+				</button>
+			</li>
+			<li>
+				<img
+					src="https://http2.mlstatic.com/tnis-nike-free-rn-flyknit-2018-original-D_NQ_NP_609798-MLB30088101460_042019-F.jpg"
+					alt="tenis"
+				/>
+				<strong>Tênis nike free rn flyknit 2018</strong>
+				<span>R$179,00</span>
+
+				<button type="button">
+					<div>
+						<MdAddShoppingCart size={16} color="#FFF" /> 3
+					</div>
+
+					<span>ADICIONAR AO CARRINHO</span>
+				</button>
+			</li>
+			<li>
+				<img
+					src="https://http2.mlstatic.com/tnis-nike-free-rn-flyknit-2018-original-D_NQ_NP_609798-MLB30088101460_042019-F.jpg"
+					alt="tenis"
+				/>
+				<strong>Tênis nike free rn flyknit 2018</strong>
+				<span>R$179,00</span>
+
+				<button type="button">
+					<div>
+						<MdAddShoppingCart size={16} color="#FFF" /> 3
+					</div>
+
+					<span>ADICIONAR AO CARRINHO</span>
+				</button>
+			</li>
+			<li>
+				<img
+					src="https://http2.mlstatic.com/tnis-nike-free-rn-flyknit-2018-original-D_NQ_NP_609798-MLB30088101460_042019-F.jpg"
+					alt="tenis"
+				/>
+				<strong>Tênis nike free rn flyknit 2018</strong>
+				<span>R$179,00</span>
+
+				<button type="button">
+					<div>
+						<MdAddShoppingCart size={16} color="#FFF" /> 3
+					</div>
+
+					<span>ADICIONAR AO CARRINHO</span>
+				</button>
+			</li>
+			<li>
+				<img
+					src="https://http2.mlstatic.com/tnis-nike-free-rn-flyknit-2018-original-D_NQ_NP_609798-MLB30088101460_042019-F.jpg"
+					alt="tenis"
+				/>
+				<strong>Tênis nike free rn flyknit 2018</strong>
+				<span>R$179,00</span>
+
+				<button type="button">
+					<div>
+						<MdAddShoppingCart size={16} color="#FFF" /> 3
+					</div>
+
+					<span>ADICIONAR AO CARRINHO</span>
+				</button>
+			</li>
+			<li>
+				<img
+					src="https://http2.mlstatic.com/tnis-nike-free-rn-flyknit-2018-original-D_NQ_NP_609798-MLB30088101460_042019-F.jpg"
+					alt="tenis"
+				/>
+				<strong>Tênis nike free rn flyknit 2018</strong>
+				<span>R$179,00</span>
+
+				<button type="button">
+					<div>
+						<MdAddShoppingCart size={16} color="#FFF" /> 3
+					</div>
+
+					<span>ADICIONAR AO CARRINHO</span>
+				</button>
+			</li>
+		</ProductList>
+	);
 }
