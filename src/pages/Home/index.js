e3215046efe538e47667431f53a190481454d838
Felipe Marinho <felipmarinho@gmail.com>
2019-12-09 19:41:53 -0300

    Buscando informações do serviço

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,107 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
-
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 import { ProductList } from './styles';
 
-export default function Home() {
-	return (
-		<ProductList>
-			<li>
-				<img
-					src="https://http2.mlstatic.com/tnis-nike-free-rn-flyknit-2018-original-D_NQ_NP_609798-MLB30088101460_042019-F.jpg"
-					alt="tenis"
-				/>
-				<strong>Tênis nike free rn flyknit 2018</strong>
-				<span>R$179,00</span>
+export default class Home extends Component {
+	state = {
+		products: [],
+	};
 
-				<button type="button">
-					<div>
-						<MdAddShoppingCart size={16} color="#FFF" /> 3
-					</div>
+	async componentDidMount() {
+		const response = await api.get('products');
 
-					<span>ADICIONAR AO CARRINHO</span>
-				</button>
-			</li>
-			<li>
-				<img
-					src="https://http2.mlstatic.com/tnis-nike-free-rn-flyknit-2018-original-D_NQ_NP_609798-MLB30088101460_042019-F.jpg"
-					alt="tenis"
-				/>
-				<strong>Tênis nike free rn flyknit 2018</strong>
-				<span>R$179,00</span>
+		const data = response.data.map(product => ({
+			...product,
+			priceFormatted: formatPrice(product.price),
+		}));
 
-				<button type="button">
-					<div>
-						<MdAddShoppingCart size={16} color="#FFF" /> 3
-					</div>
+		this.setState({ products: data });
+	}
 
-					<span>ADICIONAR AO CARRINHO</span>
-				</button>
-			</li>
-			<li>
-				<img
-					src="https://http2.mlstatic.com/tnis-nike-free-rn-flyknit-2018-original-D_NQ_NP_609798-MLB30088101460_042019-F.jpg"
-					alt="tenis"
-				/>
-				<strong>Tênis nike free rn flyknit 2018</strong>
-				<span>R$179,00</span>
+	render() {
+		const { products } = this.state;
 
-				<button type="button">
-					<div>
-						<MdAddShoppingCart size={16} color="#FFF" /> 3
-					</div>
+		return (
+			<ProductList>
+				{products.map(product => (
+					<li key={product.id}>
+						<img src={product.image} alt={product.title} />
+						<strong>{product.title}</strong>
+						<span>{product.priceFormatted}</span>
 
-					<span>ADICIONAR AO CARRINHO</span>
-				</button>
-			</li>
-			<li>
-				<img
-					src="https://http2.mlstatic.com/tnis-nike-free-rn-flyknit-2018-original-D_NQ_NP_609798-MLB30088101460_042019-F.jpg"
-					alt="tenis"
-				/>
-				<strong>Tênis nike free rn flyknit 2018</strong>
-				<span>R$179,00</span>
+						<button type="button">
+							<div>
+								<MdAddShoppingCart size={16} color="#FFF" /> 3
+							</div>
 
-				<button type="button">
-					<div>
-						<MdAddShoppingCart size={16} color="#FFF" /> 3
-					</div>
-
-					<span>ADICIONAR AO CARRINHO</span>
-				</button>
-			</li>
-			<li>
-				<img
-					src="https://http2.mlstatic.com/tnis-nike-free-rn-flyknit-2018-original-D_NQ_NP_609798-MLB30088101460_042019-F.jpg"
-					alt="tenis"
-				/>
-				<strong>Tênis nike free rn flyknit 2018</strong>
-				<span>R$179,00</span>
-
-				<button type="button">
-					<div>
-						<MdAddShoppingCart size={16} color="#FFF" /> 3
-					</div>
-
-					<span>ADICIONAR AO CARRINHO</span>
-				</button>
-			</li>
-			<li>
-				<img
-					src="https://http2.mlstatic.com/tnis-nike-free-rn-flyknit-2018-original-D_NQ_NP_609798-MLB30088101460_042019-F.jpg"
-					alt="tenis"
-				/>
-				<strong>Tênis nike free rn flyknit 2018</strong>
-				<span>R$179,00</span>
-
-				<button type="button">
-					<div>
-						<MdAddShoppingCart size={16} color="#FFF" /> 3
-					</div>
-
-					<span>ADICIONAR AO CARRINHO</span>
-				</button>
-			</li>
-		</ProductList>
-	);
+							<span>ADICIONAR AO CARRINHO</span>
+						</button>
+					</li>
+				))}
+			</ProductList>
+		);
+	}
 }
