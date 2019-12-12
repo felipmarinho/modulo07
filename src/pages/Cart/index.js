5a7cc96e5c5dbb06e748f7acd40259bebbd5d125
Felipe Marinho <felipmarinho@gmail.com>
2019-12-12 20:16:52 -0300

    Listando produtos no carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {
 	MdRemoveCircleOutline,
 	MdAddCircleOutline,
@@ -7,7 +8,7 @@ import {
 
 import { Container, ProductTable, Total } from './styles';
 
-export default function Cart() {
+function Cart({ cart }) {
 	return (
 		<Container>
 			<ProductTable>
@@ -21,37 +22,36 @@ export default function Cart() {
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td>
-							<img
-								src="https://http2.mlstatic.com/tnis-nike-free-rn-flyknit-2018-original-D_NQ_NP_609798-MLB30088101460_042019-F.jpg"
-								alt="tenis"
-							/>
-						</td>
-						<td>
-							<strong>Tênis nike free rn flyknit 2018</strong>
-							<span>R$179,00</span>
-						</td>
-						<td>
-							<div>
+					{cart.map(product => (
+						<tr key={product.id}>
+							<td>
+								<img src={product.image} alt={product.title} />
+							</td>
+							<td>
+								<strong>{product.title}</strong>
+								<span>{product.priceFormatted}</span>
+							</td>
+							<td>
+								<div>
+									<button type="button">
+										<MdRemoveCircleOutline size={20} color="#7159c1" />
+									</button>
+									<input type="number" readOnly value={product.amount} />
+									<button type="button">
+										<MdAddCircleOutline size={20} color="#7159c1" />
+									</button>
+								</div>
+							</td>
+							<td>
+								<strong>R$ 258,80</strong>
+							</td>
+							<td>
 								<button type="button">
-									<MdRemoveCircleOutline size={20} color="#7159c1" />
+									<MdDelete size={20} color="#7159c1" />
 								</button>
-								<input type="number" readOnly value={1} />
-								<button type="button">
-									<MdAddCircleOutline size={20} color="#7159c1" />
-								</button>
-							</div>
-						</td>
-						<td>
-							<strong>R$ 258,80</strong>
-						</td>
-						<td>
-							<button type="button">
-								<MdDelete size={20} color="#7159c1" />
-							</button>
-						</td>
-					</tr>
+							</td>
+						</tr>
+					))}
 				</tbody>
 			</ProductTable>
 
@@ -66,3 +66,9 @@ export default function Cart() {
 		</Container>
 	);
 }
+
+const mapStateToProps = state => ({
+	cart: state.cart,
+});
+
+export default connect(mapStateToProps)(Cart);
